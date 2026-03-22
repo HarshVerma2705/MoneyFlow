759ded2597d4d5e7861ff8d229abc752044c902e
HarshVerma2705 <hv152775@gmail.com>
2026-03-22 11:32:26 +0530

    edits

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,9 +4,7 @@ const tokenBlackListModel = require("../models/blackList.model");
 
 async function authMiddleware(req, res, next) {
     const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
-    //req.headers.authorization   → "Bearer xyz123"
-    //.split(" ")                 → ["Bearer", "xyz123"]
-    //[1]                         → "xyz123"
+    
 
 
     if (!token) {
